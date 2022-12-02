5c6b718819148df97eb0e03ac6766ad84fa0f952
Giorbis Miguel <giorbismiguel@gmail.com>
2022-12-02 06:55:55 -0500

    creating blog

---
+++ lib/src/presentation/blocs/bloc/remote_articles_state.dart
@@ -0,0 +1,29 @@
+part of 'remote_articles_bloc.dart';
+
+abstract class RemoteArticlesState extends Equatable {
+  
+  const RemoteArticlesState(this.articles, this.noMoreData, this.error);
+  
+  final List<Article> articles;
+
+  final bool noMoreData;
+
+  final DioError error;
+
+  @override
+  List<Object> get props => [articles, noMoreData, error];
+}
+
+class RemoteArticlesLoading extends RemoteArticlesState {
+  const RemoteArticlesLoading();
+}
+
+
+class RemoteArticlesDone extends RemoteArticlesState {
+  const RemoteArticlesDone(List<Article> article, {bool noMoreData})
+      : super(articles: article, noMoreData: noMoreData);
+}
+
+class RemoteArticlesError extends RemoteArticlesState {
+  const RemoteArticlesError(DioError error) : super(error: error);
+}
